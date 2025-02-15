bcd5ffeee6cc014a4c55b767ea94ea22290f323e
mbparvezme <mbparvezme@gmail.com>
2025-02-16 02:33:41 +0600

    Tooltip completed

--- src/lib/Tooltip/Tooltip.svelte
+++ src/lib/Tooltip/Tooltip.svelte
@@ -50,26 +50,26 @@
     let triggerElement: HTMLElement[]|[] = [...document.querySelectorAll<HTMLElement>("[data-tooltip]")]
     triggerElement.forEach((element: HTMLElement) => {
       if (element) {
-        let triggerEvent = element.dataset?.tooltipEvent ?? tooltipEvent;
+        let triggerEvent = element.dataset?.tooltipEvent ?? tooltipEvent
 
         // Making trigger focusable
         if (element.tabIndex < 0){
-          element.tabIndex = 0;
+          element.tabIndex = 0
         }
 
         // Setting relative class if not available
         if(!element.classList.contains("relative")){
-          element.classList.add("relative");
+          element.classList.add("relative")
         }
 
         // Click trigger event
         if(triggerEvent == "click"){
-          element.addEventListener("click", () => COMPONENT_ITEM.classList.contains("hidden") ? createElement(element) : removeElement(element));
-          element.onblur = () => removeElement(element);
+          element.addEventListener("click", () => COMPONENT_ITEM.classList.contains("hidden") ? createElement(element) : removeElement(element))
+          element.onblur = () => removeElement(element)
         }
         else{
-          element.addEventListener("mouseenter", () => createElement(element));
-          element.onmouseleave = () => removeElement(element);
+          element.addEventListener("mouseenter", () => createElement(element))
+          element.onmouseleave = () => removeElement(element)
         }
       }
     })
@@ -80,7 +80,6 @@
     elementContent = element?.dataset?.tooltip ?? ""
     // Getting tooltip position
     const finalPosition = element?.dataset.tooltipPosition as POSITION_TYPES ?? position
-    // let finalPosition = preferredPosition;
     positionalClasses = positionClasses[finalPosition]
     // Getting tooltip animation
     animation = element?.dataset.animation as TOOLTIP_ANIMATION ?? animation
@@ -93,8 +92,8 @@
   }
 
   let resetAndToggle = (el: HTMLElement, finalPosition: POSITION_TYPES) => {
-    const rect: DOMRect = el.getBoundingClientRect();
-    const elemRect: DOMRect = COMPONENT_ITEM.getBoundingClientRect();
+    const rect: DOMRect = el.getBoundingClientRect()
+    const elemRect: DOMRect = COMPONENT_ITEM.getBoundingClientRect()
     let { top: newTop, left: newLeft } = calculatePosition(finalPosition, rect, elemRect)
     if (isOverflowing(newTop, newLeft, elemRect)) {
       const alternatePositions = ["bottom", "top", "right", "left"] as POSITION_TYPES[]
@@ -103,19 +102,19 @@
         if (!isOverflowing(newTop, newLeft, elemRect)) {
           finalPosition = pos
           positionalClasses = positionClasses[finalPosition]
-          break;
+          break
         }
       }
     }
 
     // Final check: Keep tooltip inside screen bounds
-    ({ top: newTop, left: newLeft } = keepInBounds(newTop, newLeft, elemRect));
+    ({ top: newTop, left: newLeft } = keepInBounds(newTop, newLeft, elemRect))
     // Apply position styles
     setVisibleStyle(COMPONENT_ITEM, newTop, newLeft)
   }
 
   let setInitialStyle = (el: HTMLElement) => {
-    Object.assign(el.style, {visibility: "hidden", display: "block", opacity: 0, top: "-9999px", left: "-9999px"});
+    Object.assign(el.style, {visibility: "hidden", display: "block", opacity: 0, top: "-9999px", left: "-9999px"})
   }
 
   const isOverflowing = (top: number, left: number, elemRect: DOMRect) => {
@@ -135,33 +134,33 @@
         return {
           top: rect.top - offset - elemRect.height,
           left: rect.left + rect.width / 2 - elemRect.width / 2,
-        };
+        }
       case "bottom":
         return {
           top: rect.bottom + offset,
           left: rect.left + rect.width / 2 - elemRect.width / 2,
-        };
+        }
       case "left":
         return {
           top: rect.top + rect.height / 2 - elemRect.height / 2,
           left: rect.left - elemRect.width - offset,
-        };
+        }
       case "right":
         return {
           top: rect.top + rect.height / 2 - elemRect.height / 2,
           left: rect.right + offset,
-        };
+        }
     }
   }
 
   let setVisibleStyle = (el: HTMLElement, newTop: number, newLeft: number) => {
-    Object.assign(el.style, {top: `${newTop}px`, left: `${newLeft}px`, visibility: "visible", opacity: 1});
+    Object.assign(el.style, {top: `${newTop}px`, left: `${newLeft}px`, visibility: "visible", opacity: 1})
   }
 
   let removeElement = (element: HTMLElement) => {
     if(COMPONENT_ITEM.classList.contains('open')) element.removeChild(COMPONENT_ITEM)
     COMPONENT_ITEM.classList.contains("hidden")
-    Object.assign(COMPONENT_ITEM.style, {visibility: "hidden"});
+    Object.assign(COMPONENT_ITEM.style, {visibility: "hidden"})
   }
 </script>
 
