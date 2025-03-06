23c2c48a992156c201a23e58d50b992ba13e518b
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-03-06 14:23:33 +0600

    rc55

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <Container class="py-0 flex justify-center items-center h-screen text-xl">
+  <a href="/p">Test</a>
   <p>Read the <a href="https://theui.dev" target="_blank" class="underline decoration-dotted decoration-2 underline-offset-2">documentation</a>!</p>
 </Container>
