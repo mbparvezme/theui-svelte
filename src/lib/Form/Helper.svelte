dcabcedfaf7a79182776d426430d2db79d30ba15
mbparvezme <mbparvezme@gmail.com>
2025-03-11 01:53:12 +0600

    Working on form and inputs

--- src/lib/Form/Helper.svelte
+++ src/lib/Form/Helper.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	import type { Snippet } from "svelte"
   import { twMerge } from "tailwind-merge"
-  let {children, text, ...props} : {text ?: string|Snippet, [key: string] : unknown} = $props()
+  let {children, content, ...props} : {content ?: string|Snippet, [key: string] : unknown} = $props()
 </script>
 
+{#if content}
 <p {...props} class={twMerge("text-sm text-gray-500 dark:text-gray-600", props?.class as string)}>
-  {#if text}
-    {#if typeof text == "string"}
-      {@html text}
-    {/if}
-    {#if typeof text == "function"}
-      {@render text?.()}
-    {/if}
+  {#if typeof content == "string"}
+    {@html content}
+  {/if}
+  {#if typeof content == "function"}
+    {@render content()}
   {/if}
 </p>
+{/if}
