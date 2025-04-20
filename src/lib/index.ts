d960b874fdc051afa8ba1b88e3c62b4bed1c3140
mbparvezme <mbparvezme@gmail.com>
2025-04-21 02:23:31 +0600

    config and presets modified

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
-// Reexport your entry components here
+export { default as Accordion } from "./Accordion/Accordion.svelte";
+export { default as AccordionItem } from "./Accordion/AccordionItem.svelte";
