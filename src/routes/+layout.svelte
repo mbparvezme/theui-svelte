fc854a27a94c1bd6fb5d16cb5456541a9f1f376b
mbparvezme <mbparvezme@gmail.com>
2025-05-25 01:35:30 +0600

    v2.0.4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-	import '../app.css';
-	import { Tooltip, Notification } from "$lib";
-
-	let { children } = $props();
+	import '../app.css'
+	let { children } = $props()
 </script>
-
-<Tooltip />
-<Notification />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,1 @@
-<script lang="ts">
-  import {Accordion, AccordionItem, DarkMode } from "$lib";
-</script>
-
-<DarkMode />
-
-<br>
-
-<div class="p-28">
-  <Accordion>
-    <AccordionItem title="Hello 1">
-      This is accordion 1..
-    </AccordionItem>
-    <AccordionItem title="Hello 2" open>
-      This is accordion 2..
-    </AccordionItem>
-  </Accordion>
-</div>
+TheUI Svelte Component Library!
